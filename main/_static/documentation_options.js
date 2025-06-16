58069708d0f5024ac93e3eed520eddd7839a63a9
hyperrealist <hyperrealist@users.noreply.github.com>
2025-06-16 09:05:32 +0000

    deploy: 5eb9815bbb9489c18424d5c552ad19a4263bce80

---
+++ main/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.1.dev774+g5eb9815',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
