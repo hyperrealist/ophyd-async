673afca16e2ad7da60faaf2101a4c5b24bcb6d25
hyperrealist <hyperrealist@users.noreply.github.com>
2025-09-19 14:31:58 +0000

    deploy: bbf534fc5aee2918bfb85a6a8e0fa3364e88e42d

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.13.3.dev25+g893af5a76',
+    VERSION: '0.13.3.dev26+gbbf534fc5',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
